4eb980f56bc1d52e90e3b3c30ccfbebad5150de6
Kachanovskii <vadimskyi1@gmail.com>
2024-12-25 08:23:16 +0200

    Implement product and category fetching with dynamic rendering in Home and Product pages

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,27 @@ import { TopBar } from "@/components/shared/top-bar";
 import { Filters } from "@/components/shared/filters";
 import { ProductCard } from "@/components/shared/product-card";
 import { ProductsGroupList } from "@/components/shared/products-group-list";
+import { prisma } from "@/prisma/prisma-client";
 
-export default function Home() {
+export default async function Home() {
+  const catagories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        },
+      },
+    },
+  });
+
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
       </Container>
 
-      <TopBar />
+      <TopBar categories={catagories.filter((category) => category.products.length > 0)} />
 
       <Container className="mt-10 pb-14">
         <div className="flex gap-[60px]">
@@ -19,114 +31,17 @@ export default function Home() {
             <Filters />
           </div>
           <div className="flex flex-col gap-16">
-            <ProductsGroupList
-              title="Пиццы"
-              items={[
-                {
-                  id: 0,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 1,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 2,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 3,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 4,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 5,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-              ]}
-              categoryId={1}
-            />
-            <ProductsGroupList
-              title="Комбо"
-              items={[
-                {
-                  id: 0,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 1,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 3,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 4,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 6,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-                {
-                  id: 7,
-                  name: "Пицца",
-                  imageUrl:
-                    "https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg",
-                  price: 550,
-                  items: [{ price: 550 }],
-                },
-              ]}
-              categoryId={2}
-            />
+            {catagories.map(
+              (category) =>
+                category.products.length > 0 && (
+                  <ProductsGroupList
+                    key={category.id}
+                    title={category.name}
+                    categoryId={category.id}
+                    items={category.products}
+                  />
+                )
+            )}
             {/* <ProductCard id={0} name={'Пицца'} price={550} imageUrl={'https://la.ua/rivne/wp-content/uploads/sites/9/2024/11/napoli-new-3.jpg'} /> */}
           </div>
         </div>
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,40 @@
-export default function ProductPage({
+import { Container, ProductImage, Title } from "@/components/shared";
+import { GroupVariants } from "@/components/shared/group-variants";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+
+export default async function ProductPage({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <p>Product {id}</p>;
+  const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container classNmae="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} size={40} />
+
+        <div className="w-[490px] bg-[#FCFCFC] p-7">
+          <Title text={product.name} size="md" className="font-extrabold mb-1" />
+          <p className="text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, voluptatem!</p>
+          <GroupVariants items={[{
+            name: 'Маленькая',
+            value: '1',
+          }, {
+            name: 'Средняя',
+            value: '2',
+          }, {
+            name: 'Большая',
+            value: '3',
+            disabled: true,
+          }]} selectedValue="2" />
+        </div>
+      </div>
+    </Container>
+  );
 }
