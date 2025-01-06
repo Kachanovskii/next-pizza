36ea4b3f04b2295db0448aac4a9950cf4880b3e9
Kachanovskii <vadimskyi1@gmail.com>
2025-01-06 05:27:12 +0200

    Implement cart API route and state management; add cart item type and fetch functionality

---
+++ shared/store/cart.ts
@@ -0,0 +1,28 @@
+export type ICartItem = {
+  id: number;
+  quantity: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+  pizzaSize?: number | null;
+  type?: number | null;
+  ingredients: Array<{ name: string; price: number }>;
+};
+
+export interface CartState {
+  loading: boolean;
+  error: boolean;
+  totalAmount: number;
+  items: ICartItem[];
+  fetchCartItems: () => Promise<void>;
+  updateItemQuantity: (id, number, quantity, number) => Promise<void>;
+  addCartItem: (values: createCartItemValues) => Promise<void>;
+  removeCartItem: (id: number) => Promice<void>;
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+    items: [],
+    error: false,
+    loading: true,
+    totalAmount: 0,
+}))
